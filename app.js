881d28fa108ef0383ab891119bcf5d8746790b6b
samuel nascimento <samuelnasc-dev@gmail.com>
2025-02-20 19:01:48 -0300

    feat: add functions add friends to list and update

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listName = [];
+
+function getInputValue(){
+    const friendName = document.getElementById("amigo").value;
+    console.log(friendName);
+    return friendName;
+}
+
+function addFriendName() {
+    const name = getInputValue();
+    if(name){
+        listName.push(name);
+        document.getElementById('amigo').value = '';
+        console.log(listName);
+        updateList();
+    }
+}
+
+function updateList(){
+    const list = document.getElementById('listaAmigos');
+    list.innerHTML = '';
+    listName.forEach(names => {
+        const li = document.createElement('li');
+        li.textContent = names;
+        list.appendChild(li);
+    })
+}
