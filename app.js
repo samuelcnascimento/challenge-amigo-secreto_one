2552c0cfa446b341f719d68a54e75acf440c2df4
samuel nascimento <samuelnasc-dev@gmail.com>
2025-02-20 19:12:18 -0300

    feat: add function choose randomly the secret friend

--- app.js
+++ app.js
@@ -2,28 +2,38 @@
 
 let listName = [];
 
-function getInputValue(){
-    const friendName = document.getElementById("amigo").value;
-    console.log(friendName);
-    return friendName;
+function getInputValue() {
+  const friendName = document.getElementById("amigo").value;
+  console.log(friendName);
+  return friendName;
 }
 
 function addFriendName() {
-    const name = getInputValue();
-    if(name){
-        listName.push(name);
-        document.getElementById('amigo').value = '';
-        console.log(listName);
-        updateList();
-    }
+  const name = getInputValue();
+  if (name) {
+    listName.push(name);
+    document.getElementById("amigo").value = "";
+    console.log(listName);
+    updateList();
+  }
 }
 
-function updateList(){
-    const list = document.getElementById('listaAmigos');
-    list.innerHTML = '';
-    listName.forEach(names => {
-        const li = document.createElement('li');
-        li.textContent = names;
-        list.appendChild(li);
-    })
+function updateList() {
+  const list = document.getElementById("listaAmigos");
+  list.innerHTML = "";
+  listName.forEach((names) => {
+    const li = document.createElement("li");
+    li.textContent = names;
+    list.appendChild(li);
+  });
+}
+
+function chooseRandomly() {
+  if (listName.length > 0) {
+    const selectIndex = Math.floor(Math.random() * listName.length);
+    const selectName = listName[selectIndex];
+    document.getElementById(
+      "resultado"
+    ).textContent = `O amigo secreto sorteado é: ${selectName}`;
+  }
 }
